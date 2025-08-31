ac4a12d11ec6e407d965b7b5421c3444284f1d95
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-08-31 19:19:27 +0530

    tonepicker

--- server/index.js
+++ server/index.js
@@ -207,7 +207,7 @@ app.use('*', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
 });
 
